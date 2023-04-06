fad8478d6b5781c836e33f3a878e87bbb014e1e5
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-06 17:19:18 -0400

    added classes for pagination

---
+++ BACKEND/Blog.Core.Application/Helpers/QueryableHelper.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Core.Application.Parameters;
+
+namespace Blog.Core.Application.Helpers
+{
+    public static class QueryableHelper
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, PaginationRequest request)
+        {
+            return query.Skip((request.NumPage - 1) * request.Record).Take(request.Record);
+        }
+    }
+}
---
+++ BACKEND/Blog.Core.Application/Parameters/PaginationRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace Blog.Core.Application.Parameters
+{
+    public class PaginationRequest
+    {
+        public int NumPage = 1;
+        public int NumRecordPage = 10;
+        readonly int NumMaxRecordPage = 30;
+        public string Order = "asc";
+        public string? Sort = null; 
+        
+        
+
+        public int Record
+        {
+            get => NumRecordPage;
+            set
+            {
+                NumRecordPage = value > NumMaxRecordPage ? NumMaxRecordPage : value;
+            }
+        }
+    }
+}
