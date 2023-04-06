8008d65e1e5e25fcc7128f4754e59c44ab7f92ab
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-06 17:21:07 -0400

    Repository for categories

---
+++ BACKEND/Blog.Core.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Blog.Core.Domain.Entities;
+
+namespace Blog.Core.Application.Interfaces.Repository
+{
+    public interface ICategoryRepository : IGeneryRepository<Category>
+    {
+    }
+}
---
+++ BACKEND/Blog.Infrastructure.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Core.Application.Interfaces.Repository;
+using Blog.Core.Domain.Entities;
+using Blog.Infrastructure.Persistence.Context;
+
+namespace Blog.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : GeneryRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BlogContext context): base(context) { }
+    }
+}
