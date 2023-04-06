cb2b9d0f72e77ba32efaf2a3065af0ae330c8e46
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-06 17:28:55 -0400

    Adding controller and versioning for endpoints

---
+++ BACKEND/Blog.Presentation.WebApi/Controllers/v1/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Core.Application.Features.Categories.Commands.CreateCategoryCommand;
+using Blog.Core.Application.Features.Categories.Commands.DeleteCategoryCommand;
+using Blog.Core.Application.Features.Categories.Commands.UpdateCategoryCommand;
+using Blog.Core.Application.Features.Categories.Queries.GetAllCategoryQuery;
+using Blog.Core.Application.Features.Categories.Queries.GetCategoryByIdQuery;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Blog.Presentation.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class CategoryController : BaseApiController
+    {
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllCategoryQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            return Ok(await Mediator.Send(new GetCategoryByIdQuery() { CategoryId = id }));
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register(CreateCategoryCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut("Update")]
+        public async Task<ActionResult> Update(UpdateCategoryCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new DeleteCategoryCommand() { CategoryId = id}));
+        }
+    }
+}
---
+++ BACKEND/Blog.Presentation.WebApi/Extensions/VersionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Presentation.WebApi.Extensions
+{
+    public static class VersionExtension
+    {
+        public static IServiceCollection AddVersioningExtension(this IServiceCollection services)
+        {
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1,0);
+
+                config.AssumeDefaultVersionWhenUnspecified = true;
+
+                config.ReportApiVersions = true;
+            });
+
+            return services;
+        }
+    }
+}
