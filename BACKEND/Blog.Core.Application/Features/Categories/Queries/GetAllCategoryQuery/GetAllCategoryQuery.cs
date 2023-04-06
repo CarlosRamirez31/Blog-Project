a57e296e0423d98288f79031777c9c4a63e0de29
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-06 17:25:21 -0400

    Query to show all categories and by id

---
+++ BACKEND/Blog.Core.Application/Features/Categories/Queries/GetAllCategoryQuery/GetAllCategoryQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Blog.Core.Application.Dtos.Category;
+using Blog.Core.Application.Interfaces.Repository;
+using Blog.Core.Application.Wrappers;
+using MediatR;
+
+namespace Blog.Core.Application.Features.Categories.Queries.GetAllCategoryQuery
+{
+    public class GetAllCategoryQuery : IRequest<Response<List<CategoryResponseDto>>>
+    {
+
+    }
+
+    public class GetAllCategoryQueryHandler : IRequestHandler<GetAllCategoryQuery, Response<List<CategoryResponseDto>>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllCategoryQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CategoryResponseDto>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.GetAllAsync();
+
+            var dto = _mapper.Map<List<CategoryResponseDto>>(category);
+
+            return new Response<List<CategoryResponseDto>>(dto);
+        }
+    }
+}
---
+++ BACKEND/Blog.Core.Application/Features/Categories/Queries/GetCategoryByIdQuery/GetCategoryByIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Blog.Core.Application.Dtos.Category;
+using Blog.Core.Application.Interfaces.Repository;
+using Blog.Core.Application.Wrappers;
+using MediatR;
+
+namespace Blog.Core.Application.Features.Categories.Queries.GetCategoryByIdQuery
+{
+    public class GetCategoryByIdQuery : IRequest<Response<CategoryResponseDto>>
+    {
+        public int CategoryId { get; set; }
+    }
+
+    public class GetCategoryByIdQueryHandler : IRequestHandler<GetCategoryByIdQuery, Response<CategoryResponseDto>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoryByIdQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<CategoryResponseDto>> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.GetByIdAsync(request.CategoryId);
+            if (category is null)
+                throw new KeyNotFoundException($"No existe una categoria de id {request.CategoryId}");
+
+            var dto = _mapper.Map<CategoryResponseDto>(category);
+            return new Response<CategoryResponseDto>(dto);
+        }
+    }
+}
