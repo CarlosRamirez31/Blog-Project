555a0e47ee4e5a33580249889370f3eae4b10811
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-05 23:46:14 -0400

    Adding generic repository

---
+++ BACKEND/Blog.Core.Application/Interfaces/Repository/IGeneryRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Core.Application.Interfaces.Repository
+{
+    public interface IGeneryRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(T entity);
+    }
+}
