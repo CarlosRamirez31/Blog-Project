36504cb9a40f089f21e432af3673a7caf5e47936
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-07 23:31:47 -0400

    New class to filter

---
+++ BACKEND/Blog.Core.Application/Parameters/FilterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Core.Application.Parameters
+{
+    public class FilterRequest : PaginationRequest
+    {
+        public int? NumFilter { get; set; }
+        public string? TextFilter { get; set; }
+        public string? StarDate { get; set; }
+        public string? EndDate { get; set; }
+        public bool? Download { get; set; }
+    }
+}
