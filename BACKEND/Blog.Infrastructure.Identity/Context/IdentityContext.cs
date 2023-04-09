433a35de7fbc85889720fa901a9b143c6432d6f2
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-08 21:42:37 -0400

    Adding the DbContext to the IdentityContext class

---
+++ BACKEND/Blog.Infrastructure.Identity/Context/IdentityContext.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure.Identity.Context
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> option): base(option) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");
+
+            builder.Entity<ApplicationUser>().ToTable(name: "User");
+
+            builder.Entity<IdentityRole>().ToTable(name: "Role");
+
+            builder.Entity<IdentityUserRole<string>>().ToTable(name: "UserRole");
+
+            builder.Entity<IdentityUserLogin<string>>().ToTable(name: "UserLogin");
+        }
+    }
+}
