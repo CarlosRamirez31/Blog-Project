f1dfe789d9961d2041fed53c443098f2e3c6092d
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-17 10:47:53 -0400

    Add Validations to register and authenticate

---
+++ BACKEND/Blog.Core.Application/Features/Users/Commands/AuthenticationCommand/AuthenticationCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Blog.Core.Application.Features.Users.Commands.AuthenticationCommand
+{
+    public class AuthenticationCommandValidator : AbstractValidator<AuthenticationCommand>
+    {
+        public AuthenticationCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("{PropertyValue} no es valido, por favor ingrese un correo valido")
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo");
+
+            RuleFor(x => x.Password)
+                .MinimumLength(6).WithMessage("{PropertyName} no puede se menor a {MinLength}")
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo");
+        }
+    }
+}
---
+++ BACKEND/Blog.Core.Application/Features/Users/Commands/RegisterCommand/RegisterCommandValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+
+namespace Blog.Core.Application.Features.Users.Commands.RegisterCommand
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo")
+                .MaximumLength(80).WithMessage("{PropertyName} no puede se mayor a {MaxLegth}");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo")
+                .MaximumLength(120).WithMessage("{PropertyName} no puede se mayor a {MaxLegth}");
+
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("{PropertyValue} no es valido, por favor ingrese un correo valido")
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo");
+
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo")
+                .MaximumLength(80).WithMessage("{PropertyName} no puede se mayor a {MaxLegth}");
+
+            RuleFor(x => x.Password)
+                .MinimumLength(6).WithMessage("{PropertyName} no puede se menor a {MinLength}")
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password).WithMessage("{PropertyName} deben ser iguales")
+                .MinimumLength(6).WithMessage("{PropertyName} no puede se menor a {MinLength}")
+                .NotEmpty().WithMessage("{PropertyName} no puede esta vacio")
+                .NotNull().WithMessage("{PropertyName} no puede ser nulo");
+        }
+    }
+}
