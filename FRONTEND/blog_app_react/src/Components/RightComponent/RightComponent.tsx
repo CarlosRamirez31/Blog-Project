accbfae780ed9bfe2d824546db914c3764abcdbe
henny <mingito.2017@gmail.com>
2023-05-24 20:09:37 -0400

    Cambios estilos right component

--- FRONTEND/blog_app_react/src/Components/RightComponent/RightComponent.tsx
+++ FRONTEND/blog_app_react/src/Components/RightComponent/RightComponent.tsx
@@ -1,3 +1,13 @@
+import "./RightStyles.css";
+
 export const RightComponent = () => {
-  return <></>;
+  return (
+    <>
+      <div className="all_right_css_component">
+        <div className="css-1dbjc4n r-kemksi r-1kqtdi0 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08x">
+
+        </div>
+      </div>
+    </>
+  );
 };
