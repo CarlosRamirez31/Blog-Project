0cf041a2d48fd293a5be8462c38abdd72f10b5fb
Henny Feliz <mingito.2017@gmail.com>
2023-05-28 17:33:54 -0400

    Cambios

---
+++ FRONTEND/blog_app_react/src/Components/BodyComponent/Center/TweetsElements.ts
@@ -0,0 +1,26 @@
+
+
+
+interface TweetElement {
+  user_icon: string;
+  user: string;
+  posted_date: string;
+  message: string;
+  comments: string;
+  shared: string;
+  likes: string;
+}
+
+
+
+export const Element: TweetElement = [
+  {
+    user_icon: "https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png",
+    user: "",
+    posted_date: "",
+    message: "",
+    comments: "",
+    shared: "",
+    likes: ""
+  }
+]
